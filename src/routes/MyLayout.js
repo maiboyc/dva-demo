a875b420bce07d42f3e999e05b4340d5b82556e6
maiboyc <maiboyc@gmail.com>
2017-08-15 17:56:08 +0800

    修改config.js

--- src/routes/MyLayout.js
+++ src/routes/MyLayout.js
@@ -1,27 +1,88 @@
 import React from 'react';
-import { connect } from 'dva';
-import ProductList from '../components/ProductList';
-import MainLayout from '../components/MainLayout/MainLayout';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Link } from 'dva/router';
 import styles from './MyLayout.less';
 
-const MyLayout = ({ dispatch, products, location }) => {
-  function handleDelete(id) {
-    dispatch({
-      type: 'products/delete',
-      payload: id,
-    });
-  }
+const { SubMenu } = Menu;
+const { Header, Footer, Sider, Content } = Layout;
+
+function MyLayout({ location }) {
   return (
-    <MainLayout location={location}>
-      <div className={styles.normal}>
-        <h2>商品列表</h2>
-        <ProductList onDelete={handleDelete} products={products} />
-      </div>
-    </MainLayout>
+    <div className={styles.normal}>
+      <Layout>
+        <Header className="header">
+          <div className={styles.logo} />
+          <Menu
+            selectedKeys={[location.pathname]}
+            mode="horizontal"
+            theme="dark"
+          >
+            <Menu.Item key="/">
+              <Link to="/index"><Icon type="home" />主页</Link>
+            </Menu.Item>
+            <Menu.Item key="/users">
+              <Link to="/users"><Icon type="user" />用户</Link>
+            </Menu.Item>
+            <Menu.Item key="/products">
+              <Link to="/products"><Icon type="hdd" />产品</Link>
+            </Menu.Item>
+            <Menu.Item key="/myLayout">
+              <Link to="/myLayout"><Icon type="meh" />布局</Link>
+            </Menu.Item>
+            <Menu.Item key="/404">
+              <Link to="/pageNotFound"><Icon type="frown-circle" />404</Link>
+            </Menu.Item>
+            <Menu.Item key="/antd">
+              <a href="https://github.com/dvajs/dva" target="_blank" rel="noopener noreferrer">dva</a>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} style={{ background: '#fff' }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                <Menu.Item key="9">option9</Menu.Item>
+                <Menu.Item key="10">option10</Menu.Item>
+                <Menu.Item key="11">option11</Menu.Item>
+                <Menu.Item key="12">option12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '12px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+        <Footer> footer </Footer>
+      </Layout>
+    </div>
   );
+}
+
+MyLayout.propTypes = {
 };
 
-// export default Products;
-export default connect(({ products }) => ({
-  products,
-}))(MyLayout);
+export default MyLayout;
